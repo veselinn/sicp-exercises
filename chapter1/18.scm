c1b624b67438c570de370a0b3f32a8bcc93191b1
veselinn <vesoNikolov@gmail.com>
2012-03-10 19:24:29 +0200

    Adds the solutions of exercises 1.17 and 1.18

---
+++ chapter1/18.scm
@@ -0,0 +1,19 @@
+; Exercise 1.18: 
+; Using the results of Exercise 1.16 and Exercise 1.17,
+; devise a procedure that generates an iterative process for multiply-
+; ing two integers in terms of adding, doubling, and halving and uses
+; a logarithmic number of steps.
+
+(define (double a) (+ a a))
+
+(define (halve a) (/ a 2))
+
+(define (fast-* a b)
+  (define (fast-*-iter a b sum)
+    (cond
+      ((= b 0) sum)
+      ((even? b) (fast-*-iter (double a) (halve b) sum))
+      (else (fast-*-iter a (- b 1) (+ a sum)))))
+  (fast-*-iter a b 0))
+
+(write (fast-* 4 6))
