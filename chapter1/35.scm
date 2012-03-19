cf0a761f4a7d2cf9fcdef5d6e5a93084dffb0f72
veselinn <vesoNikolov@gmail.com>
2012-03-19 23:27:36 +0200

    Adds a short proof for φ.

--- chapter1/35.scm
+++ chapter1/35.scm
@@ -3,6 +3,14 @@
 ; point of the transformation x → 1 + 1/x , and use this fact to com-
 ; pute φ by means of the fixed-point procedure.
 
+; Let f(x) = 1 + 1/x, φ = (1 + √5) / 2
+; f((1 + √5)/2) = 1 + 2/(1 + √5) = 
+; (1 + √5 + 2)/(1 + √5) = | * (1 - √5)/(1 - √5)
+; (3 + √5)(1 - √5) / (1 + √5)(1 - √5) =
+; (3 - 3√5 + √5 - 5) / (1 - 5) = 
+; (-2 - 2√5) / -4 =
+; (1 + √5) / 2 = φ
+
 (define tolerance 0.00001)
 
 (define (fixed-point f first-guess)
@@ -17,5 +25,3 @@
         (try next))))
 
   (try first-guess))
-
-(display (fixed-point (lambda (x) (+ 1 (/ 1 x))) 1.6))
